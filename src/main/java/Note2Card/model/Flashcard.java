9e2b2f6d4c4bb016518e32a64032a8827cdfe0db
Emelyne1024 <emelyneishimwe749@gmail.com>
2026-04-29 15:33:21 +0200

    Writed codes for Flashcard.java model

--- src/main/java/Note2Card/model/Flashcard.java
+++ src/main/java/Note2Card/model/Flashcard.java
@@ -1,4 +1,23 @@
 package Note2Card.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Flashcard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String question;
+
+    @Column(columnDefinition = "TEXT")
+    private String answer;
+
+    @ManyToOne
+    @JoinColumn(name = "note_id")
+    private Note note;
 }
