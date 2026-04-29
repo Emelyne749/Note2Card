374b6e879373ef0b072d68c623fcc97baa84c34f
Emelyne1024 <emelyneishimwe749@gmail.com>
2026-04-29 15:03:20 +0200

    Writed codes for Note.java model

--- src/main/java/Note2Card/model/Note.java
+++ src/main/java/Note2Card/model/Note.java
@@ -1,4 +1,18 @@
 package Note2Card.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
 }
