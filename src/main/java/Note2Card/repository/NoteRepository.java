5187138ac9d1a92c396ca1e499969219e11b74ab
Emelyne1024 <emelyneishimwe749@gmail.com>
2026-04-30 10:06:41 +0200

    Writed codes for NoteRepository.java repository

--- src/main/java/Note2Card/repository/NoteRepository.java
+++ src/main/java/Note2Card/repository/NoteRepository.java
@@ -1,4 +1,10 @@
 package Note2Card.repository;
 
-public class NoteRepository {
+import Note2Card.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
 }
