29054fbb94136c2a18c497a03a22320e586ef2d8
Niyonsaba123-hub <niyojean321@gmail.com>
2026-04-30 10:53:44 +0200

    Flashcrepo  initialization

--- src/main/java/Note2Card/repository/FlashcardRepository.java
+++ src/main/java/Note2Card/repository/FlashcardRepository.java
@@ -1,4 +1,14 @@
 package Note2Card.repository;
 
-public class FlashcardRepository {
+import Note2Card.model.Flashcard;
+import Note2Card.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FlashcardRepository extends JpaRepository<Flashcard, Long> {
+
+    List<Flashcard> findByNote(Note note);
 }
